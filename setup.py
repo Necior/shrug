7c6970f488406744de107cba8ef68037a2535254
Adrian Sadłocha <adrian.adek@gmail.com>
2018-05-03 00:54:22 +0200

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     author='Adrian Sadłocha',
     description='Shrug emoticon printer',
     entry_points={'console_scripts': ['shrug = shrug.cli:main']},
+    long_description='Shrug emoticon printer',
     name='shrug',
     packages=['shrug'],
     tests_require=['pytest', 'pytest-runner'],
     url='https://github.com/Necior/shrug',
-    version='0.1.0',
+    version='0.1.1',
 )
